39dc05474e5957625276b59f3d0fbe0558ef417c
ncgege <8160903@qq.com>
2017-01-25 17:30:31 +0800

    xzc
    xzc

---
+++ install_xzc.sh
@@ -0,0 +1,3 @@
+sudo git clone https://github.com/ocminer/cpuminer-xzc
+sudo apt-get install automake autoconf pkg-config libcurl4-openssl-dev libjansson-dev libssl-dev libgmp-dev
+
---
+++ run_xzc.sh
@@ -0,0 +1,1 @@
+screen -s xzc2 /home/ubuntu/cpuminer-xzc/cpuminer  -a lyra2rev2 -o stratum+tcp://xzc.suprnova.cc:5596 -u eson.t1 -p t1
